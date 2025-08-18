89f8b4e1d24014bb627716143a2e536199a333f9
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2025-08-18 22:35:50 +0200

    wygryze?

--- Config/App.php
+++ Config/App.php
@@ -18,7 +18,7 @@ class App extends BaseConfig
      */
 
     public string $baseURL = 'https://johari.testujac.pl';
-
+     
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
